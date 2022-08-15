7d8ab5a629a06bf182cfbdf3346522ef9ff98562
popenchuk_roman <popenchuk.roman@gmail.com>
2022-08-15 21:55:05 +0300

    ch01 add reduser and dispatch. Change state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux/es/exports';
 
 function App() {
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash)
+  const addCash = () => {
+      dispatch({type: "ADD_CASH", payload: 5})
+
+  }
+  const getCash = () => {
+    dispatch({type: "GET_CASH", payload: 5})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{display: 'flex'}}>
+        <div style={{fontSize: '3rem'}}>{cash}</div>
+          <button onClick={()=>addCash()}>Пополнить счет</button>
+          <button onClick={()=>getCash()}>Снять со счета</button>
+      </div>     
     </div>
   );
 }
